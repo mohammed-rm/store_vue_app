4ccd75659a73c45fd753c1ddcf579a63f13f2d95
Mohammed R <mohammed.rmich@etu.univ-tours.fr>
2022-11-12 10:45:41 +0100

    final tests

--- project/cypress/e2e/manage_users.cy.js
+++ project/cypress/e2e/manage_users.cy.js
@@ -8,10 +8,10 @@ describe("Manage users", () => {
         cy.get("#password1").type("admindev")
         cy.get("#connection-button").click()
         cy.url().should("include", "/")
+        cy.visit("http://localhost:8080/admin")
     })
 
     it("should create a new user : admin account", function () {
-        cy.visit("http://localhost:8080/admin")
         cy.get("#myModal").should("be.visible").click()
         cy.get("#new-user-name").type(`TestUser_${number}`)
         cy.get("#new-user-forname").type(`Account_${number}`)
@@ -21,6 +21,7 @@ describe("Manage users", () => {
         cy.get("#new-user-pw").type("testuseraccount")
         cy.contains("Submit").should("be.visible").click()
 
+        cy.wait(8000)
         cy.visit("http://localhost:8080/admin")
         cy.contains("tr > td", `TestUser_${number}`).should("be.visible")
         cy.contains("tr > td", `Account_${number}`).should("be.visible")
@@ -38,10 +39,12 @@ describe("Manage users", () => {
     });
 
     it("should edit created user account : admin account", function () {
+        cy.contains("tr > td", `TestUser_${number}`).should("be.visible").click()
         cy.contains("Edit").should("be.visible").click()
         cy.get("#input-field-name").clear().type("TestUserEdited")
         cy.get("#input-field-forname").clear().type("AccountEdited")
         cy.get("#input-field-matricule").clear().type("SE-654321")
+        cy.contains("Save").should("be.visible").click()
     });
 
     it("should delete user's profile : admin account", function () {
@@ -59,6 +62,8 @@ describe("Manage users", () => {
         cy.get('#email1').type('usertest@gmail.com')
         cy.get('#password1').type('usertest')
         cy.get('#connection-button').click()
-        cy.visit('http://localhost:8080/admin')
+        cy.visit('http://localhost:8080/admin').should('not.exist')
     });
 })
+
+
